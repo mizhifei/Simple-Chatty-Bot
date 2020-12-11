6c1b2008c62c35b3cd7c895d7478c9b37b2e6989
MiZhiFei <278184634@qq.com>
2020-12-11 16:54:32 +0800

    first commit

---
+++ Problems/Area of parallelogram/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        countAreaOfParallelogram(a, b);
+    }
+
+    // Do not change code below   
+    public static void countAreaOfParallelogram(int b, int h) {
+        System.out.println(b * h);
+    }
+}
